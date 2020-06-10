2601e485646e10bc3fdf5350b7a7e14dd2396218
Connor Cumming <connorc0405@gmail.com>
2020-06-10 17:27:53 -0400

    Initial commit

---
+++ query_secret_str_external.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# Take a recording, then replay and analyze
+from os import remove, path
+from panda import Panda, blocking
+import subprocess
+
+panda = Panda(arch='x86_64', mem='1G', qcow='/panda_resources/bionic-work.qcow2', expect_prompt=rb'root@ubuntu:.*# ', extra_args='-display none -net user,hostfwd=tcp::8080-:80 -net nic')
+
+# Make sure we're always saving a new recording
+recording_name = "record_query_secret_str"
+for f in [recording_name+"-rr-nondet.log", recording_name+"-rr-snp"]:
+    if path.isfile(f): remove(f)
+
+@blocking
+def record_curl(): # Run a non-deterministic command at the root snapshot, then end .run()
+    panda.revert_sync('root')
+    print('Fixing guest network')
+    panda.run_serial_cmd('/root/fix_network.sh')
+    panda.run_monitor_cmd("begin_record {}".format(recording_name))
+    print('Running CURL from Docker')
+    subprocess.run(['curl', 'localhost:8080/cgi-bin/querystr.cgi?SECRE'])
+    panda.run_monitor_cmd("end_record")
+    panda.stop_run()
+
+print("======== TAKE RECORDING ========")
+panda.queue_async(record_curl) # Take a recording
+panda.run()
+print("======== END RECORDING ========")
+
+# print("======== RUN REPLAY ========")
+# panda.run_replay(recording_name) # Load and run the replay
+# print("======== FINISH REPLAY ========")
+
+#print("======= RUN AGAIN ======")
+#panda.queue_async(second_cmd)
+#panda.run()
+#print("======= DONE =========")
+
+
+# TODO Register python callback with tainted_branch
+# TODO then look for CGI process running
+# TODO then disassemble code.
+
