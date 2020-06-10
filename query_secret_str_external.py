89a4026b9eaac44ada9c92b1f6ebc35328218fa3
Connor Cumming <connorc0405@gmail.com>
2020-06-10 17:40:39 -0400

    Cleanup

--- query_secret_str_external.py
+++ query_secret_str_external.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
 # Take a recording, then replay and analyze
+
+
 from os import remove, path
 from panda import Panda, blocking
 import subprocess
 
-panda = Panda(arch='x86_64', mem='1G', qcow='/panda_resources/bionic-work.qcow2', expect_prompt=rb'root@ubuntu:.*# ', extra_args='-display none -net user,hostfwd=tcp::8080-:80 -net nic')
-
-# Make sure we're always saving a new recording
-recording_name = "record_query_secret_str"
-for f in [recording_name+"-rr-nondet.log", recording_name+"-rr-snp"]:
-    if path.isfile(f): remove(f)
 
 @blocking
 def record_curl(): # Run a non-deterministic command at the root snapshot, then end .run()
@@ -22,19 +18,27 @@ def record_curl(): # Run a non-deterministic command at the root snapshot, then 
     panda.run_monitor_cmd("end_record")
     panda.stop_run()
 
-print("======== TAKE RECORDING ========")
-panda.queue_async(record_curl) # Take a recording
-panda.run()
-print("======== END RECORDING ========")
 
-# print("======== RUN REPLAY ========")
-# panda.run_replay(recording_name) # Load and run the replay
-# print("======== FINISH REPLAY ========")
+def main():
+    panda = Panda(arch='x86_64', mem='1G', qcow='/panda_resources/bionic-work.qcow2', expect_prompt=rb'root@ubuntu:.*# ', extra_args='-display none -net user,hostfwd=tcp::8080-:80 -net nic')        
 
-#print("======= RUN AGAIN ======")
-#panda.queue_async(second_cmd)
-#panda.run()
-#print("======= DONE =========")
+    # Make sure we're always saving a new recording
+    recording_name = "record_query_secret_str"
+    for f in [recording_name+"-rr-nondet.log", recording_name+"-rr-snp"]:
+        if path.isfile(f): remove(f)
+    
+    print("======== TAKE RECORDING ========")
+    panda.queue_async(record_curl) # Take a recording
+    panda.run()
+    print("======== END RECORDING ========")
+
+    print("======== RUN REPLAY ========")
+    panda.run_replay(recording_name) # Load and run the replay
+    print("======== FINISH REPLAY ========")
+
+   
+if __name__ == '__main__':
+    main()
 
 
 # TODO Register python callback with tainted_branch
